b6e920efd385abbf0d2ca7ca5ad915abba1bea22
Antony <102866421+AntonyIvanovakaMEDBEDb@users.noreply.github.com>
2022-04-03 20:48:30 +0400

    Update Lsn2_3_2.py

--- Lsn2_3_2.py
+++ Lsn2_3_2.py
@@ -2,7 +2,10 @@
 # считает и выводит на консоль среднее арифметическое всех чисел из отрезка [a; b], которые кратны числу 3.
 # На вход программе подаются интервалы, внутри которых всегда есть хотя бы одно число, которое делится на 3.
 # get the  numbers using  list & split  method
-a, b = (int(i) for i in input().split())
+# a, b = (int(i) for i in input().split())
+# if we need to get numbers  step by step
+a = int(input())
+b = int(input())
 s_av = 0 #here  we  will caculate  the  average  of  list
 count_of_3multiple = 0 #here  we  will accumulate  the  count  of items multiple of 3
 for i in range(a,(b + 1)):
